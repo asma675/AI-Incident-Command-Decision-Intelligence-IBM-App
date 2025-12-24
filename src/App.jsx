78c78dfd992e55c04fa88ad4d984ff4ee95dfa9d
Asma <158836479+asma675@users.noreply.github.com>
2025-12-24 18:29:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Pages from "@/pages/index.jsx";
 import { Toaster } from "@/components/ui/toaster";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Pages />
       <Toaster />
-    </BrowserRouter>
+    </>
   );
 }
 
