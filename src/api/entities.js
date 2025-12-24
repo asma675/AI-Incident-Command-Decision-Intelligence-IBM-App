0047b76a44926adc07467f9a6f1f9b32d1688d4f
Asma <158836479+asma675@users.noreply.github.com>
2025-12-24 16:55:32 -0500

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,21 @@
+import { base44 } from './base44Client';
+
+
+export const Incident = base44.entities.Incident;
+
+export const Decision = base44.entities.Decision;
+
+export const AuditLog = base44.entities.AuditLog;
+
+export const PredictiveAlert = base44.entities.PredictiveAlert;
+
+export const PostIncidentReview = base44.entities.PostIncidentReview;
+
+export const IncidentAutomation = base44.entities.IncidentAutomation;
+
+export const KnowledgeBaseArticle = base44.entities.KnowledgeBaseArticle;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ src/api/functions.js
@@ -0,0 +1,13 @@
+import { base44 } from './base44Client';
+
+
+export const generatePredictions = base44.functions.generatePredictions;
+
+export const generatePostIncidentReview = base44.functions.generatePostIncidentReview;
+
+export const automateIncidentResponse = base44.functions.automateIncidentResponse;
+
+export const suggestKnowledgeArticles = base44.functions.suggestKnowledgeArticles;
+
+export const generateArticleFromIncident = base44.functions.generateArticleFromIncident;
+
